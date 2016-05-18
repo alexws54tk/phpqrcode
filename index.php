8934077d29a589202e7ff082175b506003f81848
alexws54tk <alex@ws54.tk>
2016-05-18 21:02:30 +0600

    Причёсывание кода
    Signed-off-by: alexws54tk <alex@ws54.tk>

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php    
+<?php
 /*
  * PHP QR Code encoder
  *
@@ -21,74 +21,68 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-    
-    echo "<h1>PHP QR Code</h1><hr/>";
-    
-    //set it to writable location, a place for temp generated PNG files
-    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
-    
-    //html PNG location prefix
-    $PNG_WEB_DIR = 'temp/';
 
-    include "qrlib.php";    
-    
-    //ofcourse we need rights to create temp dir
-    if (!file_exists($PNG_TEMP_DIR))
-        mkdir($PNG_TEMP_DIR);
-    
-    
-    $filename = $PNG_TEMP_DIR.'test.png';
-    
-    //processing form input
-    //remember to sanitize user input in real-life solution !!!
-    $errorCorrectionLevel = 'L';
-    if (isset($_REQUEST['level']) && in_array($_REQUEST['level'], array('L','M','Q','H')))
-        $errorCorrectionLevel = $_REQUEST['level'];    
+	echo "<h1>PHP QR Code</h1><hr/>";
 
-    $matrixPointSize = 4;
-    if (isset($_REQUEST['size']))
-        $matrixPointSize = min(max((int)$_REQUEST['size'], 1), 10);
+	//set it to writable location, a place for temp generated PNG files
+	$PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+
+	//html PNG location prefix
+	$PNG_WEB_DIR = 'temp/';
+
+	require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."qrlib.php");
+
+	//ofcourse we need rights to create temp dir
+	if (!file_exists($PNG_TEMP_DIR))
+		mkdir($PNG_TEMP_DIR);
 
 
-    if (isset($_REQUEST['data'])) { 
-    
-        //it's very important!
-        if (trim($_REQUEST['data']) == '')
-            die('data cannot be empty! <a href="?">back</a>');
-            
-        // user data
-        $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
-        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
-        
-    } else {    
-    
-        //default data
-        echo 'You can provide data in GET parameter: <a href="?data=like_that">like that</a><hr/>';    
-        QRcode::png('PHP QR Code :)', $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
-        
-    }    
-        
-    //display generated file
-    echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" /><hr/>';  
-    
-    //config form
-    echo '<form action="index.php" method="post">
-        Data:&nbsp;<input name="data" value="'.(isset($_REQUEST['data'])?htmlspecialchars($_REQUEST['data']):'PHP QR Code :)').'" />&nbsp;
-        ECC:&nbsp;<select name="level">
-            <option value="L"'.(($errorCorrectionLevel=='L')?' selected':'').'>L - smallest</option>
-            <option value="M"'.(($errorCorrectionLevel=='M')?' selected':'').'>M</option>
-            <option value="Q"'.(($errorCorrectionLevel=='Q')?' selected':'').'>Q</option>
-            <option value="H"'.(($errorCorrectionLevel=='H')?' selected':'').'>H - best</option>
-        </select>&nbsp;
-        Size:&nbsp;<select name="size">';
-        
-    for($i=1;$i<=10;$i++)
-        echo '<option value="'.$i.'"'.(($matrixPointSize==$i)?' selected':'').'>'.$i.'</option>';
-        
-    echo '</select>&nbsp;
-        <input type="submit" value="GENERATE"></form><hr/>';
-        
-    // benchmark
-    QRtools::timeBenchmark();    
+	$filename = $PNG_TEMP_DIR.'test.png';
 
-    
+	//processing form input
+	//remember to sanitize user input in real-life solution !!!
+	$errorCorrectionLevel = ((isset($_REQUEST['level']) && in_array($_REQUEST['level'], array('L','M','Q','H')))?$_REQUEST['level']:'L');
+
+	$matrixPointSize = ((isset($_REQUEST['size']))?min(max((int)$_REQUEST['size'], 1), 10):4);
+
+
+	$data = "PHP QR Code :)";
+	$data = ((isset($_REQUEST['data'])&&trim(htmlspecialchars($_REQUEST['data'])) != '')?trim(htmlspecialchars($_REQUEST['data'])):'PHP QR Code :)');
+	QRcode::png($data, $filename, $errorCorrectionLevel, $matrixPointSize);
+
+	//display generated file
+	echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" alt="PNG1 QR Code image" />';
+	echo '<img src="imgpng.php?data='.$data.'&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG2 QR Code image" />';
+	echo '<img src="imgsvg.php?data='.$data.'&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="SVG QR Code image" /><hr/>';
+
+	//config form
+	echo '<form action="index.php" method="post">
+		Data:&nbsp;<input type="text" name="data" value="'.$data.'" placeholder="PHP QR Code :)">&nbsp;
+		ECC:&nbsp;<select name="level">
+			<option value="L"'.(($errorCorrectionLevel=='L')?' selected':'').'>L - smallest</option>
+			<option value="M"'.(($errorCorrectionLevel=='M')?' selected':'').'>M</option>
+			<option value="Q"'.(($errorCorrectionLevel=='Q')?' selected':'').'>Q</option>
+			<option value="H"'.(($errorCorrectionLevel=='H')?' selected':'').'>H - best</option>
+		</select>&nbsp;
+		Size:&nbsp;<select name="size">';
+
+	for($i=1;$i<=10;$i++)
+		echo '<option value="'.$i.'"'.(($matrixPointSize==$i)?' selected':'').'>'.$i.'</option>';
+
+	echo '</select>&nbsp;
+		<input type="submit" value="GENERATE"></form><hr/>';
+	echo '<h2>Examples of Data formats</h2>'."\n";
+	echo '<table>
+	<tr><th>Type of QR Code</th><th>Sample text</th><th>Sample image</th></tr>
+	<tr><th>Plain text:</th><td> PHP QR Code :) </td><td><img src="imgpng.php?data=PHP QR Code :)&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG QR Code sample text" /></td></tr>
+	<tr><th>URL:</th><td> http://www.nspu.ru/ </td><td><img src="imgpng.php?data=http://www.nspu.ru/&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG QR Code sample url" /></td></tr>
+	<tr><th>SMS:</th><td> smsto:79998765432:Text of your message; </td><td><img src="imgpng.php?data=smsto:79232271844:Hello&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG QR Code sample sms" /></td></tr>
+	<tr><th>Geolocation:</th><td> geo:54.9812,82.8124,400 </td><td><img src="imgpng.php?data=geo:54.9812,82.8124,400&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG QR Code sample geo" /></td></tr>
+	<tr><th>Wi-Fi:</th><td> wifi:s:Network_Name;t:wpa;p:Password;;</td><td><img src="imgpng.php?data=wifi:s:ST_NSPU;t:wpa;p:VmfK53pSit;;&amp;level='.$errorCorrectionLevel.'&amp;size='.$matrixPointSize.'" alt="PNG QR Code sample wi-fi" /></td></tr>
+	</table>
+	<hr />';
+
+	// benchmark
+	QRtools::timeBenchmark();
+	// QRtools::buildCache();
+?>
--- qrvect.php
+++ qrvect.php
@@ -21,16 +21,16 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
- 
+
     define('QR_VECT', true);
 
     class QRvect {
-    
+
         //----------------------------------------------------------------------
-        public static function eps($frame, $filename = false, $pixelPerPoint = 4, $outerFrame = 4,$saveandprint=FALSE, $back_color = 0xFFFFFF, $fore_color = 0x000000, $cmyk = false) 
+        public static function eps($frame, $filename = false, $pixelPerPoint = 4, $outerFrame = 4,$saveandprint=FALSE, $back_color = 0xFFFFFF, $fore_color = 0x000000, $cmyk = false)
         {
             $vect = self::vectEPS($frame, $pixelPerPoint, $outerFrame, $back_color, $fore_color, $cmyk);
-            
+
             if ($filename === false) {
                 header("Content-Type: application/postscript");
                 header('Content-Disposition: filename="qrcode.eps"');
@@ -46,19 +46,17 @@
                 }
             }
         }
-        
-    
+
         //----------------------------------------------------------------------
         private static function vectEPS($frame, $pixelPerPoint = 4, $outerFrame = 4, $back_color = 0xFFFFFF, $fore_color = 0x000000, $cmyk = false) 
         {
             $h = count($frame);
             $w = strlen($frame[0]);
-            
+
             $imgW = $w + 2*$outerFrame;
             $imgH = $h + 2*$outerFrame;
-            
-            if ($cmyk)
-            {
+
+            if ($cmyk) {
                 // convert color value into decimal eps format
                 $c = round((($fore_color & 0xFF000000) >> 16) / 255, 5);
                 $m = round((($fore_color & 0x00FF0000) >> 16) / 255, 5);
@@ -72,9 +70,7 @@
                 $y = round((($back_color & 0x0000FF00) >> 8) / 255, 5);
                 $k = round(($back_color & 0x000000FF) / 255, 5);
                 $back_color_string = $c.' '.$m.' '.$y.' '.$k.' setcmykcolor'."\n";
-            }
-            else
-            {
+            } else {
                 // convert a hexadecimal color code into decimal eps format (green = 0 1 0, blue = 0 0 1, ...)
                 $r = round((($fore_color & 0xFF0000) >> 16) / 255, 5);
                 $b = round((($fore_color & 0x00FF00) >> 8) / 255, 5);
@@ -87,9 +83,8 @@
                 $g = round(($back_color & 0x0000FF) / 255, 5);
                 $back_color_string = $r.' '.$b.' '.$g.' setrgbcolor'."\n";
             }
-            
-            $output = 
-            '%!PS-Adobe EPSF-3.0'."\n".
+
+            $output = '%!PS-Adobe EPSF-3.0'."\n".
             '%%Creator: PHPQrcodeLib'."\n".
             '%%Title: QRcode'."\n".
             '%%CreationDate: '.date('Y-m-d')."\n".
@@ -97,24 +92,24 @@
             '%%LanguageLevel: 2'."\n".
             '%%Pages: 1'."\n".
             '%%BoundingBox: 0 0 '.$imgW * $pixelPerPoint.' '.$imgH * $pixelPerPoint."\n";
-            
+
             // set the scale
             $output .= $pixelPerPoint.' '.$pixelPerPoint.' scale'."\n";
             // position the center of the coordinate system
-            
+
             $output .= $outerFrame.' '.$outerFrame.' translate'."\n";
-           
-           
-            
-            
+
+
+
+
             // redefine the 'rectfill' operator to shorten the syntax
             $output .= '/F { rectfill } def'."\n";
-            
+
             // set the symbol color
             $output .= $back_color_string;
             $output .= '-'.$outerFrame.' -'.$outerFrame.' '.($w + 2*$outerFrame).' '.($h + 2*$outerFrame).' F'."\n";
-            
-            
+
+
             // set the symbol color
             $output .= $fore_color_string;
 
@@ -129,18 +124,18 @@
                     }
                 }
             }
-            
-            
+
+
             $output .='%%EOF';
-            
+
             return $output;
         }
-        
+
         //----------------------------------------------------------------------
         public static function svg($frame, $filename = false, $pixelPerPoint = 4, $outerFrame = 4,$saveandprint=FALSE, $back_color, $fore_color) 
         {
             $vect = self::vectSVG($frame, $pixelPerPoint, $outerFrame, $back_color, $fore_color);
-            
+
             if ($filename === false) {
                 header("Content-Type: image/svg+xml");
                 //header('Content-Disposition: attachment, filename="qrcode.svg"');
@@ -156,18 +151,18 @@
                 }
             }
         }
-        
-    
+
+
         //----------------------------------------------------------------------
         private static function vectSVG($frame, $pixelPerPoint = 4, $outerFrame = 4, $back_color = 0xFFFFFF, $fore_color = 0x000000) 
         {
             $h = count($frame);
             $w = strlen($frame[0]);
-            
+
             $imgW = $w + 2*$outerFrame;
             $imgH = $h + 2*$outerFrame;
-            
-            
+
+
             $output = 
             '<?xml version="1.0" encoding="utf-8"?>'."\n".
             '<svg version="1.1" baseProfile="full"  width="'.$imgW * $pixelPerPoint.'" height="'.$imgH * $pixelPerPoint.'" viewBox="0 0 '.$imgW * $pixelPerPoint.' '.$imgH * $pixelPerPoint.'"
@@ -179,19 +174,19 @@
             '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">'."\n".
             '<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" xmlns:xlink="http://www.w3.org/1999/xlink" width="'.$imgW * $pixelPerPoint.'" height="'.$imgH * $pixelPerPoint.'" viewBox="0 0 '.$imgW * $pixelPerPoint.' '.$imgH * $pixelPerPoint.'">'."\n".
             '<desc></desc>'."\n";
-                
+
             if(!empty($back_color)) {
                 $backgroundcolor = str_pad(dechex($back_color), 6, "0", STR_PAD_LEFT);
                 $output .= '<rect width="'.$imgW * $pixelPerPoint.'" height="'.$imgH * $pixelPerPoint.'" fill="#'.$backgroundcolor.'" cx="0" cy="0" />'."\n";
             }
-                
+
             $output .= 
             '<defs>'."\n".
             '<rect id="p" width="'.$pixelPerPoint.'" height="'.$pixelPerPoint.'" />'."\n".
             '</defs>'."\n".
             '<g fill="#'.str_pad(dechex($fore_color), 6, "0", STR_PAD_LEFT).'">'."\n";
-                
-                
+
+
             // Convert the matrix into pixels
 
             for($i=0; $i<$h; $i++) {
@@ -206,9 +201,8 @@
             $output .= 
             '</g>'."\n".
             '</svg>';
-            
+
             return $output;
         }
     }
-    
-    
+?>
